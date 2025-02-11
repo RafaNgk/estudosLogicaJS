d282286b5061d9f57b4a0eb01a6d6d24d27b269b
Rafael Carvalho <rafaelngcarvalho@hotmail.com>
2025-02-11 15:59:18 -0300

    projeto inicial

---
+++ exercicios.js
@@ -0,0 +1,12 @@
+let numero1 = parseInt(prompt('Digite o número que deseja elevar ao quadrado:'));
+
+function quadrado(numero1) {
+    let resultado = numero1 * numero1;
+    return resultado;
+}
+
+alert(quadrado(numero1));
+
+let langs = ['javascript', 'java', 'python'];
+
+console.log(langs[2]);
